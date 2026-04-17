846ae41295c2e91e6aafb0a9184375c7f6de5ba3
YashPatel4268 <142138437+YashPatel4268@users.noreply.github.com>
2026-04-17 14:38:28 +0530

    feat: add contact listing, real-time validation, and password strength indicator

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
 
@@ -9,8 +8,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
-
-
+// React Contact Form
 Route::get('/', [ContactController::class, 'index']);
 Route::post('/contact-store', [ContactController::class, 'store']);
+
+// List all submitted contacts
+Route::get('/contacts', [ContactController::class, 'list']);
